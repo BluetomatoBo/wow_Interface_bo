99d24efab003ae095ea2c5fa6b1acc6cb063b41b
bluetomatobo <bo@clargia.org>
2013-03-14 19:22:56 +0800

    Bagnon更新到最新
    Bagnon更新到最新

--- Bagnon_VoidStorage/localization/it.lua
+++ Bagnon_VoidStorage/localization/it.lua
@@ -4,11 +4,11 @@
 
 local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-VoidStorage', 'itIT', true)
 
-L.ConfirmTransfer = 'Depositando questi oggetti verranno rimosse tutte le modifiche e renderà gli oggetti non più commerciabili e non rimborsabili.|n|nVuoi continuare?'
+L.ConfirmTransfer = 'Depositando questi oggetti verranno rimosse tutte le modifiche e renderÃ  gli oggetti non piÃ¹ commerciabili e non rimborsabili.|n|nVuoi continuare?'
 L.PurchaseDialog = 'Vuoi attivare il servizio di Banca Eterea?|n|n|cffffd200Costo:|r %s'
 L.CannotPurchaseDialog = 'Non hai abbastanza soldi per attivare il servizio di Banca Eterea|n|n|cffff2020Costo: %s|r'
-L.AskMafia = 'CChiedi alla Mafia'
+L.AskMafia = 'Chiedi alla Mafia'
 
-L.Title = [[Banca Eteraea di %s']]
+L.Title = [[Banca Eterea di %s']]
 L.NumDeposit = 'Deposito %d'
 L.NumWithdraw = 'Prelievo %d'
