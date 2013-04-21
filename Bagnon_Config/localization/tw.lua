14e863d10a1f06c92fe8d1cdb4b77e30cbb1638a
bluetomatobo <bo@clargia.org>
2013-04-21 17:24:53 +0800

    Bagnin更新到5.2.3
    Bagnin更新到5.2.3

--- Bagnon_Config/localization/tw.lua
+++ Bagnon_Config/localization/tw.lua
@@ -1,15 +1,6 @@
 --[[
-THIS FILE IS ENCODED IN UTF-8
-
-Bagnon Config Localization Information: Chinese Traditional
-2008/12/01 by yleaf@cwdg(yaroot@gmail.com)
-2009/06/19 by 狂飆@cwdg(networm@qq.com)
-2011/07/06 by Seraveegd@鬼霧峰
-2011/08/30 by Seraveegd@鬼霧峰
-2012/01/24 by Seraveegd@鬼霧峰
-
-Last Update: 2012/01/24 by Seraveegd@鬼霧峰
-
+	Bagnon Config Localization: Chinese Traditional
+		Credits: 狂飆@cwdg, yleaf@cwdg, Seraveegd@鬼霧峰
 --]]
 
 local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-Config', 'zhTW')
@@ -27,6 +18,7 @@ L.LockFramePositions = '鎖定位置'
 L.ShowEmptyItemSlotBackground = '顯示空的物品槽的背景'
 L.HighlightItemsByQuality = '按物品品質高亮物品'
 L.HighlightUnusableItems = '高亮無法使用的物品'
+L.HighlightSetItems = '高亮已保存為套裝的物品'
 L.HighlightQuestItems = '高亮任務物品'
 L.ReverseSlotOrdering = '反向排列'
 L.ColorItemSlotsByBagType = '按背包類型對空的物品槽上色'
