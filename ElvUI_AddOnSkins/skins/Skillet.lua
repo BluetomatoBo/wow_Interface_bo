2eb810cb92d8f0b03417ae226154ab8e42351099
bluetomatobo <bo@clargia.org>
2013-05-29 21:04:28 +0800

    ElvUI_AddOnSkins更新到1.32
    ElvUI_AddOnSkins更新到1.32

--- ElvUI_AddOnSkins/skins/Skillet.lua
+++ ElvUI_AddOnSkins/skins/Skillet.lua
@@ -24,6 +24,7 @@ function AS:SkinSkillet()
 		else
 			button = ButtonOrSpellID
 		end
+		if not button then return end
 		local texture = button:GetNormalTexture()
 		if not texture then
 			texture = _G[button:GetName().."Icon"]
@@ -48,6 +49,7 @@ function AS:SkinSkillet()
 		else
 			button = ButtonOrSpellID
 		end
+		if not button then return end
 		local texture = button:GetNormalTexture()
 		if not texture then
 			texture = _G[button:GetName().."Icon"]
