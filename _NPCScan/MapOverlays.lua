54bef16d6f3adb80c78674e98db7f66602562a5e
bluetomatobo <bo@clargia.org>
2013-10-19 18:09:51 +0800

    NPCScan 更新到 5.4.0.5
    NPCScan 更新到 5.4.0.5

--- _NPCScan/MapOverlays.lua
+++ _NPCScan/MapOverlays.lua
@@ -1,36 +1,43 @@
---[[****************************************************************************
-  * _NPCScan by Saiket                                                         *
-  * MapOverlays.lua - Integration with NPC map overlay mods.                   *
-  ****************************************************************************]]
+-------------------------------------------------------------------------------
+-- Localized Lua globals.
+-------------------------------------------------------------------------------
+local _G = getfenv(0)
 
 
-local AddOnName, _NPCScan = ...;
-local NS = LibStub( "AceEvent-3.0" ):Embed( {} );
-_NPCScan.Overlays = NS;
-
-local MESSAGE_REGISTER = "NpcOverlay_RegisterScanner";
-local MESSAGE_ADD = "NpcOverlay_Add";
-local MESSAGE_REMOVE = "NpcOverlay_Remove";
-local MESSAGE_FOUND = "NpcOverlay_Found";
+-------------------------------------------------------------------------------
+-- AddOn namespace.
+-------------------------------------------------------------------------------
+local FOLDER_NAME, private = ...
 
 
+local NS = _G.LibStub("AceEvent-3.0"):Embed({})
+private.Overlays = NS
+
+local MESSAGE_REGISTER = "NpcOverlay_RegisterScanner"
+local MESSAGE_ADD = "NpcOverlay_Add"
+local MESSAGE_REMOVE = "NpcOverlay_Remove"
+local MESSAGE_FOUND = "NpcOverlay_Found"
 
 
---- Announces to overlay mods that _NPCScan will take over control of shown paths.
-function NS.Register ()
-	NS:SendMessage( MESSAGE_REGISTER, AddOnName );
+-- Announces to overlay mods that _NPCScan will take over control of shown paths.
+function NS.Register()
+	NS:SendMessage(MESSAGE_REGISTER, FOLDER_NAME)
 end
 
 
---- Enables overlay maps for a given NPC ID.
-function NS.Add ( NpcID )
-	NS:SendMessage( MESSAGE_ADD, NpcID, AddOnName );
+-- Enables overlay maps for a given NPC ID.
+function NS.Add(NpcID)
+	NS:SendMessage(MESSAGE_ADD, NpcID, FOLDER_NAME)
 end
---- Disables overlay maps for a given NPC ID.
-function NS.Remove ( NpcID )
-	NS:SendMessage( MESSAGE_REMOVE, NpcID, AddOnName );
+
+
+-- Disables overlay maps for a given NPC ID.
+function NS.Remove(NpcID)
+	NS:SendMessage(MESSAGE_REMOVE, NpcID, FOLDER_NAME)
 end
---- Lets overlay mods know the NPC ID was found.
-function NS.Found ( NpcID )
-	NS:SendMessage( MESSAGE_FOUND, NpcID, AddOnName );
+
+
+-- Lets overlay mods know the NPC ID was found.
+function NS.Found(NpcID)
+	NS:SendMessage(MESSAGE_FOUND, NpcID, FOLDER_NAME)
 end
