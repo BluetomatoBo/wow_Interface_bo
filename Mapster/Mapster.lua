c3d1430577607006e79932cfd0f1ad145110ecd4
bluetomatobo <bo@clargia.org>
2013-10-30 19:28:59 +0800

    Mapster 更新到 1.5.6.1
    Mapster 更新到 1.5.6.1

--- Mapster/Mapster.lua
+++ Mapster/Mapster.lua
@@ -89,6 +89,10 @@ function Mapster:OnInitialize()
 	self.UIHider:Hide()
 
 	self:SetupOptions()
+
+	-- hack: fix for taint errors when opening the map
+	-- come one Blizzard, fix that thing.
+	setfenv(WorldMapFrame_OnShow, setmetatable({ UpdateMicroButtons = function() end }, { __index = _G }))
 end
 
 local realZone
