0de0bd40e33fa79733bad55b13956e8855dad489
bluetomatobo <bo@clargia.org>
2014-01-26 20:34:11 +0800

    DBM-ProvingGrounds-Mop更新到 R23
    DBM-ProvingGrounds-Mop更新到 R23

--- DBM-ProvingGrounds-MoP/PGWhiteTiger.lua
+++ DBM-ProvingGrounds-MoP/PGWhiteTiger.lua
@@ -1,17 +1,22 @@
-local mod	= DBM:NewMod("d640", "DBM-ProvingGrounds-MoP")
+﻿local mod	= DBM:NewMod("d640", "DBM-ProvingGrounds-MoP", nil, nil, function(t)
+	if( GetLocale() == "deDE") then
+		return select(2, string.match(t, "(%S+): (%S+.%S+.%S+.%S+)")) -- "Feuerprobe: Tempel des Weißen Tigers QUEST nil"
+	else
+		return select(2, string.match(t, "(%S+.%S+): (%S+.%S+)")) or select(2, string.match(t, "(%S+.%S+):(%S+.%S+)"))
+	end
+end)
 local L		= mod:GetLocalizedStrings()
-local sndWOP	= mod:NewSound(nil, "SoundWOP", true)
 
-mod:SetRevision(("$Revision: 10 $"):sub(12, -3))
+mod:SetRevision(("$Revision: 23 $"):sub(12, -3))
 mod:SetZone()
 
 --mod:RegisterCombat("scenario", 1148)
 
 mod:RegisterEvents(
-	"SPELL_CAST_START",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_APPLIED_DOSE",
-	"SPELL_CAST_SUCCESS",
+	"SPELL_CAST_START 147601 144374 144106 144401 142189 142238 145200",
+	"SPELL_AURA_APPLIED 144383 144404 145206",
+	"SPELL_AURA_APPLIED_DOSE 144383",
+	"SPELL_CAST_SUCCESS 144084 144091 144088 144086 144087 145260 142838 145198",
 	"UNIT_DIED",
 	"SCENARIO_UPDATE"
 )
@@ -23,7 +28,7 @@ local warnFlamecallerTank	= mod:NewSpellAnnounce(144091, 2)--145401 is healer ve
 local warnWindGuard			= mod:NewSpellAnnounce(144087, 3)
 local warnAmbusher			= mod:NewSpellAnnounce(144086, 4)
 local warnConquerorTank		= mod:NewSpellAnnounce(144088, 3)--145409 is healer version of mob
-----Adds spawning
+----Other Stuff
 local warnPyroBlast			= mod:NewCastAnnounce(147601, 3, 3)--Tooltip says 2 but it actually has 3 sec cast
 local warnInvokeLava		= mod:NewSpellAnnounce(144374, 3)
 local warnWindBlast			= mod:NewSpellAnnounce(144106, 4)--Threat wipe & knockback, must taunt, very important
@@ -60,46 +65,43 @@ local timerWindBlastCD		= mod:NewNextTimer(21, 144106)
 local timerPowerfulSlamCD	= mod:NewCDTimer(15, 144401)--15-17sec variation
 --Damager
 local timerAmberGlobCD		= mod:NewNextTimer(10.5, 142189)
-local timerHealIllusionCD	= mod:NewNextTimer(25, 142238)
+local timerHealIllusionCD	= mod:NewNextTimer(20, 142238)
 --Healer
 local timerAquaBombCD		= mod:NewCDTimer(12, 145206, nil, false)--12-22 second variation? off by default do to this
-local timerSonicBlastCD		= mod:NewCDTimer(8, 145200)--8-11sec variation
+local timerSonicBlastCD		= mod:NewCDTimer(6, 145200)--8-11sec variation
 
 local countdownTimer		= mod:NewCountdownFades(10, 141582)
 
 mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
 
-for i = 1, 10 do
-	mod:AddEditBoxOption("count"..i, 350, "", "sound")
-end
+local started = false
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 147601 then
+	local spellId = args.spellId
+	if spellId == 147601 then
 		warnPyroBlast:Show()
 		specWarnPyroBlast:Show(args.sourceName)
-	elseif args.spellId == 144374 then
+	elseif spellId == 144374 then
 		warnInvokeLava:Show()
 		specWarnInvokeLava:Show()
-	elseif args.spellId == 144106 and self:AntiSpam(2.5, 2) then
+	elseif spellId == 144106 and self:AntiSpam(2.5, 2) then
 		warnWindBlast:Show()
 		specWarnWindBlast:Show()
 		timerWindBlastCD:Start(args.sourceGUID)
-	elseif args.spellId == 144401 and self:AntiSpam(2.5, 3) then
+	elseif spellId == 144401 and self:AntiSpam(2.5, 3) then
 		warnPowerfulSlam:Show()
 		specWarnPowerfulSlam:Show()
 		timerPowerfulSlamCD:Start(args.sourceGUID)
-		sndWOP:Play("Interface\\AddOns\\"..DBM.Options.CountdownVoice.."\\runaway.mp3") --快躲開
-	elseif args.spellId == 142189 then
+	elseif spellId == 142189 then
 		warnAmberGlobule:Show()
 		specWarnAmberGlob:Show()
 		timerAmberGlobCD:Start(args.sourceGUID)
-	elseif args.spellId == 142238 then
+	elseif spellId == 142238 then
 		warnHealIllusion:Show()
 		specWarnHealIllusion:Show(args.sourceName)
 		timerHealIllusionCD:Start(args.sourceGUID)
-		sndWOP:Play("Interface\\AddOns\\"..DBM.Options.CountdownVoice.."\\kickcast.mp3") --快打斷
-	elseif args.spellId == 145200 then
+	elseif spellId == 145200 then
 		warnSonicBlast:Show()
 		specWarnSonicBlast:Show(args.sourceName)
 		timerSonicBlastCD:Start(args.sourceGUID)
@@ -107,12 +109,12 @@ function mod:SPELL_CAST_START(args)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 144383 and args:IsPlayer() and self:AntiSpam(1.5, 1) then
+	local spellId = args.spellId
+	if spellId == 144383 and args:IsPlayer() and self:AntiSpam(1.5, 1) then
 		specWarnInvokeLavaSIS:Show()
-		sndWOP:Play("Interface\\AddOns\\"..DBM.Options.CountdownVoice.."\\runaway.mp3") --快躲開
-	elseif args.spellId == 144404 then
+	elseif spellId == 144404 then
 		warnEnrage:Show(args.destName)
-	elseif args.spellId == 145206 then
+	elseif spellId == 145206 then
 		warnAquaBomb:Show(args.destName)
 		specWarnAquaBomb:Show(args.destName)
 		timerAquaBombCD:Start(args.sourceGUID)
@@ -134,23 +136,24 @@ mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
 "<328.4 18:05:29> [CLEU] SPELL_CAST_SUCCESS#false#0xF1311A93000006C8#Large Illusionary Flamecaller#2632#0##nil#-2147483648#-2147483648#145401#Illusionary Flamecaller#1", -- [9132]
 --]]
 function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 144084 and self:AntiSpam(2, 4) then
+	local spellId = args.spellId
+	if spellId == 144084 and self:AntiSpam(2, 4) then
 		warnRipperTank:Show()
-	elseif args.spellId == 144091 and self:AntiSpam(2, 10) then
+	elseif spellId == 144091 and self:AntiSpam(2, 10) then
 		warnFlamecallerTank:Show()
-	elseif args.spellId == 144088 and self:AntiSpam(2, 5) then
+	elseif spellId == 144088 and self:AntiSpam(2, 5) then
 		warnConquerorTank:Show()
-	elseif args.spellId == 144086 and self:AntiSpam(2, 6) then
+	elseif spellId == 144086 and self:AntiSpam(2, 6) then
 		warnAmbusher:Show()
 		specWarnAmbusher:Show()
-	elseif args.spellId == 144087 and self:AntiSpam(2, 7) then
+	elseif spellId == 144087 and self:AntiSpam(2, 7) then
 		warnWindGuard:Show()
-	elseif args.spellId == 145260 and self:AntiSpam(2, 8) then
+	elseif spellId == 145260 and self:AntiSpam(2, 8) then
 		warnBurrow:Show(args.destName)
-	elseif args.spellId == 142838 and self:AntiSpam(2, 9) then
+	elseif spellId == 142838 and self:AntiSpam(2, 9) then
 		warnBanshee:Show()
 		specWarnBanshee:Show()
-	elseif args.spellId == 145198 and self:AntiSpam(2, 11) then
+	elseif spellId == 145198 and self:AntiSpam(2, 11) then
 		warnStinger:Show()
 		specWarnStinger:Show()
 	end
@@ -176,16 +179,11 @@ end
 function mod:SCENARIO_UPDATE(newStep)
 	local diffID, currWave, maxWave, duration = C_Scenario.GetProvingGroundsInfo()
 	if diffID > 0 then
+		started = true
 		countdownTimer:Cancel()
 		countdownTimer:Start(duration)
-		local wavenum = currWave % 10
-		local inforwave = "count"..wavenum
-		if mod.Options[inforwave] then			
-			DBM.InfoFrame:SetHeader(wavenum)
-			DBM.InfoFrame:Show(1, "other", "", mod.Options[inforwave])
-		end
-	else
+	elseif started then
+		started = false
 		countdownTimer:Cancel()
-		DBM.InfoFrame:Hide()
 	end
 end
