cac038baa2e35e5f6fd68d043a749b84159b98b6
bluetomatobo <bo@clargia.org>
2014-04-14 19:26:16 +0800

    AddOnSkins_ElvUI 更新到 2.20

--- AddOnSkins_Skins/InboxMailBag.lua
+++ AddOnSkins_Skins/InboxMailBag.lua
@@ -9,12 +9,31 @@ function AS:SkinInboxMailBag(event)
 	AS:SkinEditBox(InboxMailbagFrameItemSearchBox)
 	AS:SkinCheckBox(InboxMailbagFrameItemGroupStacksCheckBox)
 	AS:SkinTab(MailFrameTab3)
+	AS:SkinScrollBar(InboxMailbagFrameScrollFrameScrollBar)
 	for i = 1, 99 do
-		local name = _G['InboxMailbagFrameItem'..i]
-		if not name then return end
-		AS:SkinIconButton(name)
+		local button = _G['InboxMailbagFrameItem'..i]
+		if not button then return end
+		button:CreateBackdrop('Default', true)
+		button:StyleButton()
+		button:DisableDrawLayer('BORDER')
+		button.icon:SetDrawLayer('ARTWORK')
+		button.icon:SetTexCoord(unpack(AS.TexCoords))
+		button.qualityOverlay:SetTexture(nil)
+		button.qualityOverlay.SetTexture = AS.Noop
+		local backdrop = button.backdrop or button.Backdrop
+		button:SetNormalTexture(nil)
+		button.SetNormalTexture = AS.Noop
+		backdrop:SetOutside(button.icon)
+		button.icon:SetParent(backdrop)
+		button:HookScript('OnUpdate', function()
+			if MAILBAGDB["QUALITY_COLORS"] and button.qualityOverlay:IsShown() and not button.searchOverlay:IsShown() then
+				backdrop:SetBackdropBorderColor(button.qualityOverlay:GetVertexColor())
+			else
+				backdrop:SetBackdropBorderColor(unpack(AS.BorderColor))
+			end
+		end)
 	end
-	AS:UnregisterEvent(name, event)
+	AS:UnregisterSkinEvent(name, event)
 end
 
 AS:RegisterSkin(name, AS.SkinInboxMailBag, 'MAIL_SHOW')
