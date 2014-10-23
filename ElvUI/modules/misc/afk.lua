75e40c0c51347710f0e68729e9620ffa682d455d
bluetomatobo <bo@clargia.org>
2014-10-23 20:10:59 +0800

    ElvUI 更新到 7.27

--- ElvUI/modules/misc/afk.lua
+++ ElvUI/modules/misc/afk.lua
@@ -10,6 +10,7 @@ function AFK:UpdateTimer()
 end
 
 function AFK:SetAFK(status)
+	if(InCombatLockdown()) then return end
 	if(status) then
 		SaveView(5);
 		MoveViewLeftStart(CAMERA_SPEED);
@@ -23,29 +24,56 @@ function AFK:SetAFK(status)
 			self.AFKMode.bottom.guild:SetText(L["No Guild"])
 		end
 
-		--self.AFKMode.bottom.model.curAnimation = "wave"
-		--self.AFKMode.bottom.model.startTime = GetTime()
-		--self.AFKMode.bottom.model.delay = 2.3
+		self.AFKMode.bottom.model.curAnimation = "wave"
+		self.AFKMode.bottom.model.startTime = GetTime()
+		self.AFKMode.bottom.model.duration = 2.3
 		self.AFKMode.bottom.model:SetUnit("player")
-		--self.AFKMode.bottom.model:SetAnimation(67)
+		self.AFKMode.bottom.model.isIdle = nil
+		self.AFKMode.bottom.model:SetAnimation(67)
+		self.AFKMode.bottom.model.idleDuration = 40
 		self.startTime = GetTime()
 		self.timer = self:ScheduleRepeatingTimer('UpdateTimer', 1)
 
 		self.isAFK = true
-	else
+	elseif(self.isAFK) then
 		UIParent:Show()
 		self.AFKMode:Hide()
 		MoveViewLeftStop();
-		SetView(5);
+		ResetView(5);
 		self:CancelTimer(self.timer)
 		self:CancelTimer(self.animTimer)
 		self.AFKMode.bottom.time:SetText("00:00")
+
+		if(PVEFrame:IsShown()) then --odd bug, frame is blank
+			PVEFrame_ToggleFrame()
+			PVEFrame_ToggleFrame()
+		end
 
 		self.isAFK = false
 	end
 end
 
-function AFK:PLAYER_FLAGS_CHANGED()
+function AFK:OnEvent(event, ...)
+	if(event == "PLAYER_REGEN_DISABLED" or event == "LFG_PROPOSAL_SHOW" or event == "UPDATE_BATTLEFIELD_STATUS") then
+		if(event == "UPDATE_BATTLEFIELD_STATUS") then
+			local status = GetBattlefieldStatus(...);
+			if ( status == "confirm" ) then
+				self:SetAFK(false)
+			end
+		else
+			self:SetAFK(false)
+		end
+
+		if(event == "PLAYER_REGEN_DISABLED") then
+			self:RegisterEvent("PLAYER_REGEN_ENABLED", "OnEvent")
+		end
+		return
+	end
+
+	if(event == "PLAYER_REGEN_ENABLED") then
+		self:UnregisterEvent("PLAYER_REGEN_ENABLED")
+	end
+
 	if(UnitIsAFK("player")) then
 		self:SetAFK(true)
 	else
@@ -56,17 +84,23 @@ end
 
 function AFK:Toggle()
 	if(E.db.general.afk) then
-		self:RegisterEvent("PLAYER_FLAGS_CHANGED")
+		self:RegisterEvent("PLAYER_FLAGS_CHANGED", "OnEvent")
+		self:RegisterEvent("PLAYER_REGEN_DISABLED", "OnEvent")
+		self:RegisterEvent("LFG_PROPOSAL_SHOW", "OnEvent")
+		self:RegisterEvent("UPDATE_BATTLEFIELD_STATUS", "OnEvent")
 		SetCVar("autoClearAFK", "1")
 	else
 		self:UnregisterEvent("PLAYER_FLAGS_CHANGED")
+		self:UnregisterEvent("PLAYER_REGEN_DISABLED")
+		self:UnregisterEvent("LFG_PROPOSAL_SHOW")
+		self:UnregisterEvent("UPDATE_BATTLEFIELD_STATUS")
 	end
 end
 
 local function OnKeyDown(self, key)
 	if(key == 'LALT') then return end
 	AFK:SetAFK(false)
-	AFK:ScheduleTimer('PLAYER_FLAGS_CHANGED', 60)
+	AFK:ScheduleTimer('OnEvent', 60)
 end
 
 function AFK:LoopAnimations()
@@ -74,26 +108,9 @@ function AFK:LoopAnimations()
 		ElvUIAFKPlayerModel:SetAnimation(69)
 		ElvUIAFKPlayerModel.curAnimation = "dance"
 		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 25
+		ElvUIAFKPlayerModel.duration = 300
 		ElvUIAFKPlayerModel.isIdle = false
-	elseif(ElvUIAFKPlayerModel.curAnimation == "dance") then
-		ElvUIAFKPlayerModel:SetAnimation(74)
-		ElvUIAFKPlayerModel.curAnimation = "roar"
-		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 3
-		ElvUIAFKPlayerModel.isIdle = false		
-	elseif(ElvUIAFKPlayerModel.curAnimation == "roar") then
-		ElvUIAFKPlayerModel:SetAnimation(70)
-		ElvUIAFKPlayerModel.curAnimation = "laugh"
-		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 0 --3.2
-		ElvUIAFKPlayerModel.isIdle = true
-	--[[elseif(ElvUIAFKPlayerModel.curAnimation == "laugh") then
-		ElvUIAFKPlayerModel:SetAnimation(100)
-		ElvUIAFKPlayerModel.curAnimation = "sleep"
-		ElvUIAFKPlayerModel.startTime = GetTime()
-		ElvUIAFKPlayerModel.delay = 0
-		ElvUIAFKPlayerModel.isIdle = true]]			
+		ElvUIAFKPlayerModel.idleDuration = 120
 	end
 end
 
@@ -107,50 +124,31 @@ function AFK:Initialize()
 	self.AFKMode:EnableKeyboard(true)
 	self.AFKMode:SetScript("OnKeyDown", OnKeyDown)
 
-	self.AFKMode.top = CreateFrame("Frame", nil, self.AFKMode)
-	self.AFKMode.top:SetTemplate("Transparent")
-	self.AFKMode.top:SetPoint("TOP", self.AFKMode, "TOP", 0, 2)
-	self.AFKMode.top:SetWidth(GetScreenWidth())
-	self.AFKMode.top:SetHeight(GetScreenHeight() * (1 / 10))
-
-	self.AFKMode.top.logo = self.AFKMode.top:CreateTexture(nil, 'OVERLAY')
-	self.AFKMode.top.logo:SetSize(384, 192)
-	self.AFKMode.top.logo:SetPoint("CENTER", self.AFKMode.top, "CENTER", 0, -50)
-	self.AFKMode.top.logo:SetTexture("Interface\\AddOns\\ElvUI\\media\\textures\\logo.tga")
-	
-
-	local factionGroup = UnitFactionGroup("player");
-	--factionGroup = "Alliance"
-	self.AFKMode.top.factionRight = self.AFKMode.top:CreateTexture(nil, 'OVERLAY')
-	self.AFKMode.top.factionRight:SetPoint("BOTTOMRIGHT", self.AFKMode.top, "BOTTOMRIGHT", -10, -16)
-	self.AFKMode.top.factionRight:SetTexture("Interface\\Timer\\"..factionGroup.."-Logo")
-	self.AFKMode.top.factionRight:SetSize(140, 140)
-
-	self.AFKMode.top.factionLeft = self.AFKMode.top:CreateTexture(nil, 'OVERLAY')
-	self.AFKMode.top.factionLeft:SetPoint("BOTTOMLEFT", self.AFKMode.top, "BOTTOMLEFT", 10, -16)
-	self.AFKMode.top.factionLeft:SetTexture("Interface\\Timer\\"..factionGroup.."-Logo")
-	self.AFKMode.top.factionLeft:SetSize(140, 140)
-
 	self.AFKMode.bottom = CreateFrame("Frame", nil, self.AFKMode)
 	self.AFKMode.bottom:SetTemplate("Transparent")
 	self.AFKMode.bottom:SetPoint("BOTTOM", self.AFKMode, "BOTTOM", 0, -2)
 	self.AFKMode.bottom:SetWidth(GetScreenWidth())
 	self.AFKMode.bottom:SetHeight(GetScreenHeight() * (1 / 10))
 
+	self.AFKMode.bottom.logo = self.AFKMode.bottom:CreateTexture(nil, 'OVERLAY')
+	self.AFKMode.bottom.logo:SetSize(320, 150)
+	self.AFKMode.bottom.logo:SetPoint("CENTER", self.AFKMode.bottom, "CENTER", 0, 50)
+	self.AFKMode.bottom.logo:SetTexture("Interface\\AddOns\\ElvUI\\media\\textures\\logo.tga")
+
 	self.AFKMode.bottom.name = self.AFKMode.bottom:CreateFontString(nil, 'OVERLAY')
-	self.AFKMode.bottom.name:SetFont(E.LSM:Fetch("font", "ElvUI Alt-Combat"), 24, "OUTLINE")
+	self.AFKMode.bottom.name:SetFont(E.LSM:Fetch("font", "ElvUI Pixel"), 20, "MONOCHROMEOUTLINE")
 	self.AFKMode.bottom.name:SetText(E.myname.."-"..E.myrealm)
 	self.AFKMode.bottom.name:SetPoint("TOPLEFT", self.AFKMode.bottom, "TOPLEFT", 20, -12)
 	self.AFKMode.bottom.name:SetTextColor(classColor.r, classColor.g, classColor.b)
 
 	self.AFKMode.bottom.guild = self.AFKMode.bottom:CreateFontString(nil, 'OVERLAY')
-	self.AFKMode.bottom.guild:SetFont(E.LSM:Fetch("font", "ElvUI Alt-Combat"), 24, "OUTLINE")
+	self.AFKMode.bottom.guild:SetFont(E.LSM:Fetch("font", "ElvUI Pixel"), 20, "MONOCHROMEOUTLINE")
 	self.AFKMode.bottom.guild:SetText(L["No Guild"])
 	self.AFKMode.bottom.guild:SetPoint("TOPLEFT", self.AFKMode.bottom.name, "BOTTOMLEFT", 0, -6)
 	self.AFKMode.bottom.guild:SetTextColor(0.7, 0.7, 0.7)
 
 	self.AFKMode.bottom.time = self.AFKMode.bottom:CreateFontString(nil, 'OVERLAY')
-	self.AFKMode.bottom.time:SetFont(E.LSM:Fetch("font", "ElvUI Alt-Combat"), 24, "OUTLINE")
+	self.AFKMode.bottom.time:SetFont(E.LSM:Fetch("font", "ElvUI Pixel"), 20, "MONOCHROMEOUTLINE")
 	self.AFKMode.bottom.time:SetText("00:00")
 	self.AFKMode.bottom.time:SetPoint("TOPLEFT", self.AFKMode.bottom.guild, "BOTTOMLEFT", 0, -6)
 	self.AFKMode.bottom.time:SetTextColor(0.7, 0.7, 0.7)
@@ -159,14 +157,14 @@ function AFK:Initialize()
 	self.AFKMode.bottom.model:SetPoint("BOTTOMRIGHT", self.AFKMode.bottom, "BOTTOMRIGHT", 90, -80)
 	self.AFKMode.bottom.model:SetSize(700, 700)
 	self.AFKMode.bottom.model:SetFacing(6)
-	--[[self.AFKMode.bottom.model:SetScript("OnUpdateModel", function(self) 
+	self.AFKMode.bottom.model:SetScript("OnUpdateModel", function(self) 
 		local timePassed = GetTime() - self.startTime
-		if(timePassed > self.delay) and self.isIdle ~= true then
+		if(timePassed > self.duration) and self.isIdle ~= true then
 			self:SetAnimation(0)
 			self.isIdle = true
-			AFK.animTimer = AFK:ScheduleTimer("LoopAnimations", 5)
+			AFK.animTimer = AFK:ScheduleTimer("LoopAnimations", self.idleDuration)
 		end
-	end)]]
+	end)
 
 	self:Toggle()
 	self.isActive = false	
