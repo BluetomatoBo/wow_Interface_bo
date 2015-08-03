ba7fa45889f947ce86140481f850176b3e859482
bluetomatobo <bo@clargia.org>
2015-08-03 19:20:21 +0800

    ElvUI Fix

--- ElvUI/modules/misc/misc.lua
+++ ElvUI/modules/misc/misc.lua
@@ -58,12 +58,10 @@ function M:MERCHANT_SHOW()
 	if cost > 0 then
 		if possible then
 			RepairAllItems(autoRepair == 'GUILD')
-			local c, s, g = cost%100, floor((cost%10000)/100), floor(cost/10000)
-
 			if autoRepair == 'GUILD' then
-				E:Print(L["Your items have been repaired using guild bank funds for: "]..GetCoinTextureString(cost, 12))
+				E:Print(L["Your items have been repaired using guild bank funds for: "]..E:FormatMoney(cost, "SMART", true)) --Amount, style, textOnly
 			else
-				E:Print(L["Your items have been repaired for: "]..GetCoinTextureString(cost, 12))
+				E:Print(L["Your items have been repaired for: "]..E:FormatMoney(cost, "SMART", true)) --Amount, style, textOnly
 			end
 		else
 			E:Print(L["You don't have enough money to repair."])
