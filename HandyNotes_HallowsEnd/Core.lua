50f095e60aa1ad1d2e09794e79f054e15fe8227c
bluetomatobo <bo@clargia.org>
2015-09-01 19:19:30 +0800

    HandyNotes_HallowEnd 更新到 1.2.1

--- HandyNotes_HallowsEnd/Core.lua
+++ HandyNotes_HallowsEnd/Core.lua
@@ -8,7 +8,7 @@
 
 
 -- declaration
-local ID, HallowsEnd = ...
+local _, HallowsEnd = ...
 HallowsEnd.points = {}
 
 
@@ -20,10 +20,18 @@ local defaults = { profile = { completed = false, icon_scale = 1.4, icon_alpha =
 -- upvalues
 local _G = getfenv(0)
 
+local C_Timer_NewTicker = _G.C_Timer.NewTicker
+local CalendarGetDate = _G.CalendarGetDate
+local CalendarGetDayEvent = _G.CalendarGetDayEvent
+local CalendarGetMonth = _G.CalendarGetMonth
+local CalendarGetNumDayEvents = _G.CalendarGetNumDayEvents
+local CalendarSetAbsMonth = _G.CalendarSetAbsMonth
 local CloseDropDownMenus = _G.CloseDropDownMenus
 local GameTooltip = _G.GameTooltip
+local GetAchievementCriteriaInfo = _G.GetAchievementCriteriaInfo
+local GetGameTime = _G.GetGameTime
+local GetQuestsCompleted = _G.GetQuestsCompleted
 local gsub = _G.string.gsub
-local IsQuestFlaggedCompleted = _G.IsQuestFlaggedCompleted
 local LibStub = _G.LibStub
 local next = _G.next
 local pairs = _G.pairs
@@ -33,12 +41,10 @@ local UIParent = _G.UIParent
 local WorldMapButton = _G.WorldMapButton
 local WorldMapTooltip = _G.WorldMapTooltip
 
-local Astrolabe = DongleStub("Astrolabe-1.0")
-local Cartographer_Waypoints = _G.Cartographer_Waypoints
 local HandyNotes = _G.HandyNotes
-local NotePoint = _G.NotePoint
 local TomTom = _G.TomTom
 
+local completedQuests = {}
 local points = HallowsEnd.points
 
 
@@ -66,18 +72,9 @@ end
 
 local function createWaypoint(_, mapFile, coord)
 	local x, y = HandyNotes:getXY(coord)
+	local m = HandyNotes:GetMapFiletoMapID(mapFile)
 
-	if TomTom then
-		local m = GetCurrentMapAreaID()
-
-		TomTom:AddMFWaypoint(m, nil, x, y, { title = "Candy Bucket" })
---		TomTom:AddZWaypoint(c, z, x * 100, y * 100, "Candy Bucket")
-	elseif Cartographer_Waypoints then
-		local c, z = HandyNotes:GetCZ(mapFile)
-		local zone = HandyNotes:GetCZToZone(c, z)
-
-		Cartographer_Waypoints:AddWaypoint( NotePoint:new(zone, x, y, "Candy Bucket") )
-	end
+	TomTom:AddMFWaypoint(m, nil, x, y, { title = "Candy Bucket" })
 end
 
 do
@@ -103,7 +100,7 @@ do
 
 			UIDropDownMenu_AddButton(info, level)
 
-			if TomTom or Cartographer_Waypoints then
+			if TomTom then
 				-- waypoint menu item
 				info.notCheckable = nil
 				info.disabled = nil
@@ -146,28 +143,15 @@ do
 end
 
 do
-	local continentMapFile = {
-		["Kalimdor"]              = {__index = Astrolabe.ContinentList[1]},
-		["Azeroth"]               = {__index = Astrolabe.ContinentList[2]},
-		["Expansion01"]           = {__index = Astrolabe.ContinentList[3]},
-		["Northrend"]             = {__index = Astrolabe.ContinentList[4]},
-		["TheMaelstromContinent"] = {__index = Astrolabe.ContinentList[5]},
-		["Vashjir"]               = {[0] = 613, 614, 615, 610},
-		["Pandaria"]              = {__index = Astrolabe.ContinentList[6]},
-	}
-
-	for k, v in pairs(continentMapFile) do
-		setmetatable(v, v)
-	end
-
 	-- custom iterator we use to iterate over every node in a given zone
 	local function iter(t, prestate)
+		if not HallowsEnd.isEnabled then return nil end
 		if not t then return nil end
 
 		local state, value = next(t, prestate)
 
 		while state do -- have we reached the end of this zone?
-			if value and (db.completed or not IsQuestFlaggedCompleted(value)) then
+			if value and (db.completed or not completedQuests[value]) then
 				return state, nil, "interface\\icons\\achievement_halloween_candy_01", db.icon_scale, db.icon_alpha
 			end
 
@@ -178,6 +162,7 @@ do
 	end
 
 	local function iterCont(t, prestate)
+		if not HallowsEnd.isEnabled then return nil end
 		if not t then return nil end
 
 		local zone = t.Z
@@ -191,7 +176,7 @@ do
 				state, value = next(data, prestate)
 
 				while state do -- have we reached the end of this zone?
-					if value and (db.completed or not IsQuestFlaggedCompleted(value)) then -- show on continent?
+					if value and (db.completed or not completedQuests[value]) then -- show on continent?
 						return state, mapFile, "interface\\icons\\achievement_halloween_candy_01", db.icon_scale, db.icon_alpha
 					end
 
@@ -200,7 +185,7 @@ do
 			end
 
 			-- get next zone
-			zone = zone + 1
+			zone = next(t.C, zone)
 			t.Z = zone
 			mapFile = HandyNotes:GetMapIDtoMapFile(t.C[zone])
 			cleanMapFile = gsub(mapFile or "", "_terrain%d+$", "")
@@ -210,10 +195,10 @@ do
 	end
 
 	function HallowsEnd:GetNodes(mapFile)
-		local C = continentMapFile[mapFile] -- Is this a continent?
+		local C = HandyNotes:GetContinentZoneList(mapFile) -- Is this a continent?
 
 		if C then
-			local tbl = { C = C, Z = 0 }
+			local tbl = { C = C, Z = next(C) }
 			return iterCont, tbl, nil
 		else
 			mapFile = gsub(mapFile, "_terrain%d+$", "")
@@ -267,24 +252,67 @@ local options = {
 }
 
 
--- initialise
-function HallowsEnd:OnEnable()
-	local _, month, day = CalendarGetDate()
+-- check
+local setEnabled = false
+local function CheckEventActive()
+	local _, month, day, year = CalendarGetDate()
+	local curMonth, curYear = CalendarGetMonth()
+	local monthOffset = -12 * (curYear - year) + month - curMonth
+	local numEvents = CalendarGetNumDayEvents(monthOffset, day)
 
-	if ( month == 10 and day >= 18 ) or ( month == 11 and day == 1 ) then
-		HandyNotes:RegisterPluginDB("HallowsEnd", self, options)
-		self:RegisterEvent("QUEST_TURNED_IN", "Refresh") -- args: questID, unknown, zero
+	for i=1, numEvents do
+		local _, eventHour, _, eventType, state, _, texture = CalendarGetDayEvent(monthOffset, day, i)
 
-		db = LibStub("AceDB-3.0"):New("HandyNotes_HallowsEndDB", defaults, "Default").profile
-	else
-		self:Disable()
+		if texture == "Calendar_HallowsEnd" then
+			if state == "ONGOING" then
+				setEnabled = true
+			else
+				local hour = GetGameTime()
+
+				if state == "END" and hour <= eventHour or state == "START" and hour >= eventHour then
+					setEnabled = true
+				else
+					setEnabled = false
+				end
+			end
+		end
+	end
+
+	if setEnabled and not HallowsEnd.isEnabled then
+		HallowsEnd.isEnabled = true
+		HallowsEnd:Refresh()
+		HallowsEnd:RegisterEvent("QUEST_TURNED_IN", "Refresh")
+
+		HandyNotes:Print("The Hallow's End celebrations have begun!  Locations of candy buckets are now marked on your map.")
+	elseif not setEnabled and HallowsEnd.isEnabled then
+		HallowsEnd.isEnabled = false
+		HallowsEnd:Refresh()
+		HallowsEnd:UnregisterAllEvents()
+
+		HandyNotes:Print("The Hallow's End celebrations have ended.  See you next year!")
 	end
 end
 
-function HallowsEnd:Refresh()
+
+-- initialise
+function HallowsEnd:OnEnable()
+	self.isEnabled = false
+
+	local _, month, _, year = CalendarGetDate()
+	CalendarSetAbsMonth(month, year)
+
+	C_Timer_NewTicker(15, CheckEventActive)
+	HandyNotes:RegisterPluginDB("HallowsEnd", self, options)
+
+	completedQuests = GetQuestsCompleted(completedQuests)
+	db = LibStub("AceDB-3.0"):New("HandyNotes_HallowsEndDB", defaults, "Default").profile
+end
+
+function HallowsEnd:Refresh(_, questID)
+	if questID then completedQuests[tonumber(questID)] = true end
 	self:SendMessage("HandyNotes_NotifyUpdate", "HallowsEnd")
 end
 
 
 -- activate
-HallowsEnd = LibStub("AceAddon-3.0"):NewAddon(HallowsEnd, ID, "AceEvent-3.0")
+LibStub("AceAddon-3.0"):NewAddon(HallowsEnd, "HandyNotes_HallowsEnd", "AceEvent-3.0")
--- HandyNotes_HallowsEnd/README.md
+++ HandyNotes_HallowsEnd/README.md
@@ -6,7 +6,7 @@ What it does
 ------------
 
 This addon displays locations of unvisited candy buckets on the worldmap and minimap.
-If you also have TomTom or Cartographer installed, you can right-click on a marker to create a Waypoint arrow.
+If you also have [TomTom](http://www.curse.com/addons/wow/tomtom), you can right-click on a marker to create a Waypoint arrow.
 
 Requires [HandyNotes](http://www.curse.com/addons/wow/handynotes) to work.
 
