e2a60d24e7b433a07709a83d3c970a2b222bcd6d
bluetomatobo <bo@clargia.org>
2015-10-13 19:08:21 +0800

    TidyPlates 更新到 6.17

--- TidyPlates/TidyPlatesCore.lua
+++ TidyPlates/TidyPlatesCore.lua
@@ -222,6 +222,63 @@ end
 --  Nameplate Extension: Applies scripts, hooks, and adds additional frame variables and regions
 ---------------------------------------------------------------------------------------------------------------------
 do
+
+--[[
+	local function DumpNameplateInfo(plate)
+		local ListChildren
+
+		local function ListRegions(object, indent)
+			local count = object:GetNumRegions()
+
+			if count > 0 then print(indent, "Regions of ", object:GetName()) end
+
+			for i = 1, count do
+				local region = select(i,object:GetRegions())
+
+				local name = region:GetName()
+				local otype = region:GetObjectType()
+				local extra = ""
+
+				if otype == "FontString" then extra = region:GetText()
+				elseif otype == "Texture" then extra = region:GetTexture()
+				end
+
+				print(indent, i, otype, name, extra)
+
+			end
+		end
+
+
+		ListChildren = function(object, indent)
+
+			local count = select('#',object:GetChildren())
+
+			if count > 0 then print(indent, "Children of ", object:GetName()) end
+
+			for i = 1, count do
+				local child = select(i,object:GetChildren())
+				local name = child:GetName()
+				local otype = child:GetObjectType()
+				local extra
+				local sublevels = child:GetNumChildren()
+				local subregions = child:GetNumRegions()
+
+				if otype == "StatusBar" then extra = child:GetStatusBarTexture() 
+				end
+
+				print(indent, i, otype, name, extra, sublevels, subregions)
+				ListRegions(child, indent.."  ")
+
+				ListChildren(child, indent.."    ") 
+			end
+		end
+
+		ListChildren(plate, "")
+
+	end
+
+	--]]
+
 	-- ApplyPlateExtesion
 	function OnNewNameplate(plate)
 		Plates[plate] = true
@@ -230,7 +287,7 @@ do
 		--------------------------------
 		local bars, regions = {}, {}
 		local bargroup, namegroup = plate:GetChildren()
-		local health, absorb, cast = bargroup:GetChildren()
+		local health, oddbar, cast = bargroup:GetChildren()
 
 		bars.health = health
 		bars.cast = cast
@@ -238,6 +295,7 @@ do
 
 		health.parentPlate = plate		-- Needed for OnHealthUpdate Hook
 		cast.parentPlate = plate		-- Needed for UpdateCastBar Hook
+
 
 		-- Region References
 		regions.threatglow,
@@ -247,10 +305,10 @@ do
 		regions.skullicon,
 		regions.raidicon,
 		regions.eliteicon
-			= bargroup:GetRegions()
+			= bargroup:GetRegions()		-- Good For 6.2
 
 		regions.name
-			= namegroup:GetRegions()
+			= namegroup:GetRegions()	-- Good For 6.2
 
 		regions.castborder,
 		regions.castnostop,
@@ -259,8 +317,15 @@ do
 		regions.spellshadow
 			= select(2, cast:GetRegions())
 
+		-- oddbar?
+			--[[
+			Regions
+				Shield-Fill
+				Shield-Overlay
+			]]
+
+
 		-- Make Blizzard Plate invisible
-		-- [[
 		health:SetStatusBarTexture(EMPTY_TEXTURE)
 		cast:SetStatusBarTexture(EMPTY_TEXTURE)
 
@@ -287,7 +352,6 @@ do
 		regions.spellshadow:SetTexture(nil)
 		regions.spellshadow:Hide()
 		regions.spelltext:Hide()
-		--]]
 
         -- Tidy Plates Frame
         --------------------------------
