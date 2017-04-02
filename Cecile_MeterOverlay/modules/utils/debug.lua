a8602d6bdd9d23ae75584b13f105b09e890ba7a8
bluetomatobo <bo@clargia.org>
2017-04-02 10:32:59 +0800

    Cecile_MeterOverlay 更新到 1.2.1

--- Cecile_MeterOverlay/modules/utils/debug.lua
+++ Cecile_MeterOverlay/modules/utils/debug.lua
@@ -15,143 +15,146 @@ function mod.ScrollingFunction(self, arg)
 
   --check if we scroll down or up and if shift is down
   if arg > 0 then
-	if IsShiftKeyDown() then 
+	if IsShiftKeyDown() then
 		self:ScrollToTop();
-	else 
+	else
 		self:ScrollUp();
 	end
   elseif arg < 0 then
-	if IsShiftKeyDown() then 
+	if IsShiftKeyDown() then
 		self:ScrollToBottom();
-	else 
+	else
 		self:ScrollDown();
 	end
   end
-  
+
 end
 
 --init the module
 function mod:OnInitialize()
 
-	--set the call for our module
-	local mt = getmetatable(mod) or {};
-	mt.__call = mod.DebugStub;
-	setmetatable(mod, mt);
+  --set the call for our module
+  local mt = getmetatable(self) or {};
+  mt.__call = self.DebugStub;
+  setmetatable(self, mt);
 
-	--module frame
-	mod.frame = CreateFrame("Frame", Engine.Name.."_DBG_Frame", UIParent);
-	local frame = mod.frame;
-	
-	--main window
-	frame:EnableMouse();
+  --module frame
+  mod.frame = _G.CreateFrame("Frame", Engine.Name.."_DBG_Frame", _G.UIParent);
+  local frame = self.frame;
 
-	frame:SetMinResize(300, 100);
-	frame:SetWidth(400);
-	frame:SetHeight(400);
+  --main window
+  frame:EnableMouse();
 
-	frame:SetPoint("CENTER", UIParent);
-	frame:SetFrameStrata("TOOLTIP");
-	frame:SetBackdrop(
-	  {
-		bgFile = "Interface\\Tooltips\\ChatBubble-Background",
-		edgeFile = "Interface\\Tooltips\\ChatBubble-BackDrop",
-		tile = true,
-		tileSize = 16,
-		edgeSize = 16,
-		insets = { left=16, right=16, top=16, bottom=16 }
-	  })
-	frame:SetBackdropColor(0, 0, 0, 1);
+  frame:SetMinResize(300, 100);
+  frame:SetWidth(400);
+  frame:SetHeight(400);
 
-	-- The titlebar/drag bar.
-	frame:SetMovable(true);
-	frame:SetClampedToScreen(true);
-	frame.title = CreateFrame("Button", nil, frame);
-	frame.title:SetNormalFontObject("GameFontNormal");
-	frame.title:SetText(Engine.Name.." Debug");
-	frame.title:SetPoint("TOPLEFT", frame, "TOPLEFT", 8, -8);
-	frame.title:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -8, -8);
-	frame.title:SetHeight(8);
-	frame.title:SetHighlightTexture(
-	  "Interface\\FriendsFrame\\UI-FriendsFrame-HighlightBar");
-	
-	--move main window
-	frame.title:SetScript("OnMouseDown",function (self) 
-			self:GetParent():StartMoving();
-		end);
-	frame.title:SetScript("OnMouseUp",function (self) 
-			self:GetParent():StopMovingOrSizing();
-		end);
-	
-	-- The sizing button.
-	frame:SetResizable(true);
-	frame.sizer = CreateFrame("Button", nil, frame);
-	frame.sizer:SetHeight(16);
-	frame.sizer:SetWidth(16);
-	frame.sizer:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT");
-	
-	--change main window size
-	frame.sizer:SetScript("OnMouseDown",function (self)
-			self:GetParent():StartSizing("BOTTOMRIGHT");
-		end);
-	frame.sizer:SetScript("OnMouseUp",function (self) 
-			self:GetParent():StopMovingOrSizing();
-		end);
+  frame:SetPoint("CENTER", _G.UIParent);
+  frame:SetFrameStrata("TOOLTIP");
+  frame:SetBackdrop(
+    {
+      bgFile = "Interface\\Tooltips\\ChatBubble-Background",
+      edgeFile = "Interface\\Tooltips\\ChatBubble-BackDrop",
+      tile = true,
+      tileSize = 16,
+      edgeSize = 16,
+      insets = { left=16, right=16, top=16, bottom=16 }
+    })
+  frame:SetBackdropColor(0, 0, 0, 1);
 
-	local line1 = frame.sizer:CreateTexture(nil, "BACKGROUND");
-	line1:SetWidth(14);
-	line1:SetHeight(14);
-	line1:SetPoint("BOTTOMRIGHT", -8, 8);
-	line1:SetTexture("Interface\\Tooltips\\UI-Tooltip-Border");
-	local x = 0.1 * 14/17;
-	line1:SetTexCoord(0.05 - x, 0.5, 0.05, 0.5 + x, 0.05, 0.5 - x, 0.5 + x, 0.5);
+  -- The title bar/drag bar.
+  frame:SetMovable(true);
+  frame:SetClampedToScreen(true);
+  frame.title = _G.CreateFrame("Button", nil, frame);
+  frame.title:SetNormalFontObject("GameFontNormal");
+  frame.title:SetText(Engine.Name.." Debug");
+  frame.title:SetPoint("TOPLEFT", frame, "TOPLEFT", 8, -8);
+  frame.title:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -8, -8);
+  frame.title:SetHeight(8);
+  frame.title:SetHighlightTexture(
+    "Interface\\FriendsFrame\\UI-FriendsFrame-HighlightBar");
 
-	local line2 = frame.sizer:CreateTexture(nil, "BACKGROUND");
-	line2:SetWidth(8);
-	line2:SetHeight(8);
-	line2:SetPoint("BOTTOMRIGHT", -8, 8);
-	line2:SetTexture("Interface\\Tooltips\\UI-Tooltip-Border");
-	local x = 0.1 * 8/17;
-	line2:SetTexCoord(0.05 - x, 0.5, 0.05, 0.5 + x, 0.05, 0.5 - x, 0.5 + x, 0.5);
+  --move main window
+  frame.title:SetScript("OnMouseDown",function (object)
+    object:GetParent():StartMoving();
+  end);
 
-	frame.bottom = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall");
-	frame.bottom:SetJustifyH("LEFT");
-	frame.bottom:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 8, 8);
-	frame.bottom:SetPoint("BOTTOMRIGHT", frame.sizer);
-	frame.bottom:SetHeight(8);
-	frame.bottom:SetText(L["DEBUG_WINDOW_HELP"]);
+  frame.title:SetScript("OnMouseUp",function (object)
+    object:GetParent():StopMovingOrSizing();
+  end);
 
-	-- The scrolling message frame with all the debug info.
-	frame.msg = CreateFrame("ScrollingMessageFrame", nil, frame);
-	frame.msg:SetPoint("TOPLEFT", frame.title, "BOTTOMLEFT");
-	frame.msg:SetPoint("TOPRIGHT", frame.title, "BOTTOMRIGHT");
-	frame.msg:SetPoint("BOTTOM", frame.bottom, "TOP", 0, 8);
+  -- The sizing button.
+  frame:SetResizable(true);
+  frame.sizer = _G.CreateFrame("Button", nil, frame);
+  frame.sizer:SetHeight(16);
+  frame.sizer:SetWidth(16);
+  frame.sizer:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT");
 
-	frame.msg:SetMaxLines(10000);
-	frame.msg:SetFading(false);
-	frame.msg:SetFontObject("GameFontHighlightLeft");
-	frame.msg:EnableMouseWheel(true);
+  --change main window size
+  frame.sizer:SetScript("OnMouseDown",function (object)
+    object:GetParent():StartSizing("BOTTOMRIGHT");
+  end);
 
-	-- Hook scrolling to scroll up down with mousewheel. shift mousewheel
-	-- scroll all the way to the top/bottom.
-	frame.msg:SetScript("OnMouseWheel", mod.ScrollingFunction);
-	
+  frame.sizer:SetScript("OnMouseUp",function (object)
+    object:GetParent():StopMovingOrSizing();
+  end);
+
+  local line1 = frame.sizer:CreateTexture(nil, "BACKGROUND");
+  line1:SetWidth(14);
+  line1:SetHeight(14);
+  line1:SetPoint("BOTTOMRIGHT", -8, 8);
+  line1:SetTexture("Interface\\Tooltips\\UI-Tooltip-Border");
+  local x = 0.1 * 14/17;
+  line1:SetTexCoord(0.05 - x, 0.5, 0.05, 0.5 + x, 0.05, 0.5 - x, 0.5 + x, 0.5);
+
+  local line2 = frame.sizer:CreateTexture(nil, "BACKGROUND");
+  line2:SetWidth(8);
+  line2:SetHeight(8);
+  line2:SetPoint("BOTTOMRIGHT", -8, 8);
+  line2:SetTexture("Interface\\Tooltips\\UI-Tooltip-Border");
+  x = 0.1 * 8/17;
+  line2:SetTexCoord(0.05 - x, 0.5, 0.05, 0.5 + x, 0.05, 0.5 - x, 0.5 + x, 0.5);
+
+  frame.bottom = frame:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall");
+  frame.bottom:SetJustifyH("LEFT");
+  frame.bottom:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 8, 8);
+  frame.bottom:SetPoint("BOTTOMRIGHT", frame.sizer);
+  frame.bottom:SetHeight(8);
+  frame.bottom:SetText(L["DEBUG_WINDOW_HELP"]);
+
+  -- The scrolling message frame with all the debug info.
+  frame.msg = _G.CreateFrame("ScrollingMessageFrame", nil, frame);
+  frame.msg:SetPoint("TOPLEFT", frame.title, "BOTTOMLEFT");
+  frame.msg:SetPoint("TOPRIGHT", frame.title, "BOTTOMRIGHT");
+  frame.msg:SetPoint("BOTTOM", frame.bottom, "TOP", 0, 8);
+
+  frame.msg:SetMaxLines(10000);
+  frame.msg:SetFading(false);
+  frame.msg:SetFontObject(_G.ChatFontNormal);
+  frame.msg:SetJustifyH("LEFT");
+  frame.msg:EnableMouseWheel(true);
+
+  -- Hook scrolling to scroll up down with mouse wheel. shift mouse wheel
+  -- scroll all the way to the top/bottom.
+  frame.msg:SetScript("OnMouseWheel", self.ScrollingFunction);
+
 	mod.isDebugging = false;
 	frame:Hide();
-	
+
 	if(Engine.Profile.debug) then
 		mod:EnableDebugging(true);
 		mod:Show(true);
 	end
-	
+
 end
 
 --enable module
-function mod:OnEnable() 
+function mod:OnEnable()
 
 end
 
 --stub default function when we are not debuging
-function mod:DebugStub(fmt, ...) 
+function mod:DebugStub(fmt, ...)
 end
 
 --get the time
@@ -176,7 +179,7 @@ function mod:GetCaller()
 
 	--return the caller, including addon name if we found it
 	return addon..trace:match("([^\\]-): in function") or trace;
-	
+
 end
 
 --write to the debug window
@@ -208,7 +211,7 @@ end
 
 --show/hide the main window
 function mod:Show(value)
-  
+
 	if(value) then
 		if not mod.frame:IsShown() then
 			mod.frame:Show();
@@ -218,5 +221,5 @@ function mod:Show(value)
 			mod.frame:Hide();
 		end
 	end
-	
+
 end
