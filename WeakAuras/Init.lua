6a31065681f970be3ee3c4019ebb5bd4e39cc44e
bluetomatobo <bo@clargia.org>
2017-07-17 19:40:25 +0800

    WeakAuras 更新到 2.4.14

--- WeakAuras/Init.lua
+++ WeakAuras/Init.lua
@@ -3,7 +3,7 @@ WeakAuras.L = {}
 
 local versionString = GetAddOnMetadata("WeakAuras", "Version");
 --[===[@debug@
-if versionString == "2.4.13" then
+if versionString == "2.4.14" then
   versionString = "Development"
 end
 --@end-debug@]===]
