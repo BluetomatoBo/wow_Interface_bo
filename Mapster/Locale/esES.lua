c21d69d42d082cfd4a3070289f22bdb0985fa515
bluetomatobo <bo@clargia.org>
2017-08-30 22:36:26 +0800

    Mapster 更新到 1.7.6

--- Mapster/Locale/esES.lua
+++ Mapster/Locale/esES.lua
@@ -9,63 +9,21 @@ L["Accuracy"] = "Precisión"
 L["Adjust the size of the Player Arrow on the Map for better visibility."] = "Ajusta el tamaño de la Flecha del Jugador en el mapa para mejorar su visibilidad."
 L["Alpha"] = "Alfa"
 L["BattleMap"] = "Mapa de Batalla"
---Translation missing 
--- L["Change the scale of the world map if you do not want the whole screen filled while the map is open."] = "Change the scale of the world map if you do not want the whole screen filled while the map is open."
 L["Coordinates"] = "Coordenadas"
 L["Cursor"] = "Cursor"
 L["Disable Mouse"] = "Deshabilitar Ratón"
---Translation missing 
--- L["Disable the mouse interactivity of the main map, eg. to change zones."] = "Disable the mouse interactivity of the main map, eg. to change zones."
---Translation missing 
--- L["EJ Icon Scale"] = "EJ Icon Scale"
 L["Enable BattleMap"] = "Habilitar Mapa de Batalla"
 L["Enable Coordinates"] = "Habilitar Coordenadas"
 L["Enable FogClear"] = "Habilitar Mapa Descubierto"
 L["Enable Group Icons"] = "Habilitar Iconos de Grupo"
---Translation missing 
--- L["Faded Alpha"] = "Faded Alpha"
 L["FogClear"] = "BorrarNiebla"
---Translation missing 
--- L["Font Size"] = "Font Size"
---Translation missing 
--- L["Font Size on the normal map."] = "Font Size on the normal map."
 L["Group Icons"] = "Iconos Grupo"
 L["Hide Map Button"] = "Ocultar Botón del Mapa"
 L["Hide Textures"] = "Ocultar Texturas"
---Translation missing 
--- L["Hide the surrounding textures around the BattleMap, only leaving you with the pure map overlays."] = "Hide the surrounding textures around the BattleMap, only leaving you with the pure map overlays."
 L["Mapster allows you to control various aspects of your World Map. You can change the style of the map, control the plugins that extend the map with new functionality, and configure different profiles for every of your characters."] = "Mapster permite controlar varios aspectos de tu Mapa del Mundo. Puedes cambiar el estilo de el mapa, controlar accesorios que amplían el mapa con nuevas funcionalidades, y configurar diferentes perfiles para cada uno de tus personajes."
 L["Overlay Color"] = "Superposición de Color"
 L["Player"] = "Jugador"
 L["PlayerArrow Scale"] = "Tamaño de la Flecha del Jugador"
---Translation missing 
--- L["POI Scale"] = "POI Scale"
 L["Scale"] = "Tamaño"
---Translation missing 
--- L["Scale of the big map."] = "Scale of the big map."
---Translation missing 
--- L["Scale of the Encounter Journal Icons on the Map."] = "Scale of the Encounter Journal Icons on the Map."
---Translation missing 
--- L["Scale of the POI Icons on the Map."] = "Scale of the POI Icons on the Map."
---Translation missing 
--- L["Size on the Battle Map"] = "Size on the Battle Map"
---Translation missing 
--- L["Size on the World Map"] = "Size on the World Map"
---Translation missing 
--- L["The BattleMap module allows you to change the style of the BattlefieldMinimap, removing unnecessary textures or PvP Objectives."] = "The BattleMap module allows you to change the style of the BattlefieldMinimap, removing unnecessary textures or PvP Objectives."
---Translation missing 
--- L["The Coordinates module adds a display of your current location, and the coordinates of your mouse cursor to the World Map frame."] = "The Coordinates module adds a display of your current location, and the coordinates of your mouse cursor to the World Map frame."
---Translation missing 
--- L["The FogClear module removes the Fog of War from the World map, thus displaying the artwork for all the undiscovered zones, optionally with a color overlay on undiscovered areas."] = "The FogClear module removes the Fog of War from the World map, thus displaying the artwork for all the undiscovered zones, optionally with a color overlay on undiscovered areas."
---Translation missing 
--- L["The Group Icons module allows you to resize the raid and party icons on the world map."] = "The Group Icons module allows you to resize the raid and party icons on the world map."
---Translation missing 
--- L["The map will fade out to the configured Fade Alpha level when you start moving."] = "The map will fade out to the configured Fade Alpha level when you start moving."
---Translation missing 
--- L["The transparency of the big map."] = "The transparency of the big map."
---Translation missing 
--- L["The transparency of the map while you are moving and the map is faded."] = "The transparency of the map while you are moving and the map is faded."
 L["You can change the transparency of the world map to allow you to continue seeing the world environment while your map is open for navigation."] = "Puedes cambiar la transparencia del mapa del mundo para poder seguir viendo el entorno aunque el mapa esté abierto."
---Translation missing 
--- L["You can control the accuracy of the coordinates, e.g. if you need very exact coordinates you can set this to 2."] = "You can control the accuracy of the coordinates, e.g. if you need very exact coordinates you can set this to 2."
 
