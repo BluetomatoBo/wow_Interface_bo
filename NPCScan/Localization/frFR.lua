3cfaf7a107478f309b00a2ff01b02eb97368fc33
bluetomatobo <bo@clargia.org>
2018-09-15 18:05:45 +0800

    NPCScan 更新到 8.0.1.13

--- NPCScan/Localization/frFR.lua
+++ NPCScan/Localization/frFR.lua
@@ -2,9 +2,9 @@ local AddOnFolderName = ...
 local L = _G.LibStub("AceLocale-3.0"):NewLocale(AddOnFolderName, "frFR")
 if not L then return end
 
-L["%1$s (%2$d) is already on the user-defined NPC list."] = "%1$s (%2$d) est déjà dans la liste des PNJ définis par l'utilisateur."
-L["%1$s (%2$d) is not on the user-defined NPC list."] = "%1$s (%2$d) n'est pas dans la liste des PNJ définis par l'utilisateur."
-L["Added %1$s (%2$d) to the user-defined NPC list."] = "%1$s (%2$d) a été ajouté à la liste des PNJ définis par l'utilisateur."
+L["%1$s (%2$d) is already on the user-defined NPC list."] = "%1$s (%2$d) est déjà dans la liste des PNJs définis par l'utilisateur."
+L["%1$s (%2$d) is not on the user-defined NPC list."] = "%1$s (%2$d) n'est pas dans la liste des PNJs définis par l'utilisateur."
+L["Added %1$s (%2$d) to the user-defined NPC list."] = "%1$s (%2$d) a été ajouté à la liste des PNJs définis par l'utilisateur."
 L["Alerts"] = "Alertes"
 L["BOTTOM"] = "Bas"
 L["BOTTOMLEFT"] = "Bas gauche"
@@ -12,21 +12,20 @@ L["BOTTOMRIGHT"] = "Bas droite"
 L["CENTER"] = "Centre"
 L["Completed Achievement Criteria"] = "Critères de hauts faits accomplis"
 L["Completed Quest Objectives"] = "Objectifs de quêtes complétées"
---[[Translation missing --]]
---[[ L["Dead NPCs"] = "Dead NPCs"--]] 
+L["Dead NPCs"] = "PNJs morts"
 L["Detection"] = "Détection"
 L["Drag to set the spawn point for targeting buttons."] = "Faire glisser pour définir le lieu d'apparition des boutons de ciblage."
 L["Duration"] = "Durée"
 L["Hide Anchor"] = "Cacher l'ancre"
-L["Hide During Combat"] = "Cacher en combat"
+L["Hide During Combat"] = "Cacher en combat "
 L["Horizontal offset from the anchor point."] = "Décalage horizontal du point d'ancrage."
 L["Ignore Mute"] = "Ignorer la sourdine"
 L["Interval"] = "Intervalle"
 L["LEFT"] = "Gauche"
 L["NPCs"] = "PNJs"
 L["Play alert sounds when sound is muted."] = "Jouer les sons d'alerte quand le son du jeu est coupé."
-L["Predefined NPCs cannot be added to or removed from the user-defined NPC list."] = "Les PNJs prédéfinis ne peuvent être ajouté ou retiré de la liste des PNJ définis par l'utilisateur."
-L["Removed %1$s (%2$d) from the user-defined NPC list."] = "%1$s (%2$d) retiré de la liste des PNJ définis par l'utilisateur."
+L["Predefined NPCs cannot be added to or removed from the user-defined NPC list."] = "Les PNJs prédéfinis ne peuvent être ajouté ou retiré de la liste des PNJs définis par l'utilisateur."
+L["Removed %1$s (%2$d) from the user-defined NPC list."] = "%1$s (%2$d) retiré de la liste des PNJs définis par l'utilisateur."
 L["Reset Position"] = "Réinitialiser postition"
 L["RIGHT"] = "Droite"
 L["Screen Flash"] = "Clignotement de l'écran"
@@ -38,10 +37,8 @@ L["The number of minutes before an NPC will be detected again."] = "Le nombre de
 L["TOP"] = "Haut"
 L["TOPLEFT"] = "Haut gauche"
 L["TOPRIGHT"] = "Haut droit"
---[[Translation missing --]]
---[[ L["Type the name of a Continent, Dungeon, or Zone, or the partial name of an NPC. Accepts Lua patterns."] = "Type the name of a Continent, Dungeon, or Zone, or the partial name of an NPC. Accepts Lua patterns."--]] 
---[[Translation missing --]]
---[[ L["Valid values are a numeric NPC ID, the word \"mouseover\" while you have your mouse cursor over an NPC, or the word \"target\" while you have an NPC set as your target."] = "Valid values are a numeric NPC ID, the word \"mouseover\" while you have your mouse cursor over an NPC, or the word \"target\" while you have an NPC set as your target."--]] 
+L["Type the name of a Continent, Dungeon, or Zone, or the partial name of an NPC. Accepts Lua patterns."] = "Entrer le nom d'un Continent, Donjon, ou Zone, ou le nom partiel d'un PNJ. Accepte les motifs Lua"
+L["Valid values are a numeric NPC ID, the word \"mouseover\" while you have your mouse cursor over an NPC, or the word \"target\" while you have an NPC set as your target."] = "Les valeurs valides sont le numéro ID d'un PNJ, le mot \"mouseover\" pendant que votre curseur survole un PNJ, ou le mot \"target\" pendant que vous ciblez un PNJ."
 L["Vertical offset from the anchor point."] = "Décalage vertical du point d'ancrage."
 L["X Offset"] = "Décalage sur X"
 L["Y Offset"] = "Décalage sur Y"
