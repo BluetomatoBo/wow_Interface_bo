2ab43cf620e39280907ff543ce4274416c9f29e7
bluetomatobo <bo@clargia.org>
2018-12-12 21:26:21 +0800

    WorldQuestTracker 更新到 8.0.1.342

--- WorldQuestTracker/locales/ruRU.lua
+++ WorldQuestTracker/locales/ruRU.lua
@@ -3,9 +3,14 @@ if not L then return end
 
 L["S_APOWER_AVAILABLE"] = "Доступно"
 L["S_APOWER_NEXTLEVEL"] = "Следующий уровень"
+L["S_DECREASESIZE"] = "Уменьшить размер"
 L["S_ENABLED"] = "Включено"
 L["S_ERROR_NOTIMELEFT"] = "Время выполнения задания истекло."
 L["S_ERROR_NOTLOADEDYET"] = "Задание ещё не загружено, подождите несколько секунд."
+--[[Translation missing --]]
+--[[ L["S_FACTION_TOOLTIP_SELECT"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_FACTION_TOOLTIP_TRACK"] = ""--]] 
 L["S_FLYMAP_SHOWTRACKEDONLY"] = "Только отслеживаемые"
 L["S_FLYMAP_SHOWTRACKEDONLY_DESC"] = "Показывать только задания, которые вы отслеживаете"
 L["S_FLYMAP_SHOWWORLDQUESTS"] = "Показывать локальные задания"
@@ -48,7 +53,7 @@ L["S_GROUPFINDER_RIGHTCLICKCLOSE"] = "ПКМ для зaкрытия"
 L["S_GROUPFINDER_SECONDS"] = "Секунд"
 L["S_GROUPFINDER_TITLE"] = "Поиск групп"
 L["S_GROUPFINDER_TUTORIAL1"] = "Делайте локальные задания быстрее, присоединившись к группам, выполняя одно и то же задание!"
-L["S_MAPBAR_AUTOWORLDMAP"] = "Автоматическая карта мира"
+L["S_INCREASESIZE"] = "Увеличить размер"
 L["S_MAPBAR_FILTER"] = "Фильтры"
 L["S_MAPBAR_FILTERMENU_FACTIONOBJECTIVES"] = "Задания фракций"
 L["S_MAPBAR_FILTERMENU_FACTIONOBJECTIVES_DESC"] = "Показывать задания фракций, даже если они скрыты."
@@ -61,7 +66,6 @@ L["S_MAPBAR_OPTIONSMENU_ARROWSPEED_SLOW"] = "Низкая частота"
 L["S_MAPBAR_OPTIONSMENU_EQUIPMENTICONS"] = "Знaчки экипировки"
 L["S_MAPBAR_OPTIONSMENU_QUESTTRACKER"] = "Включить World Quest Tracker"
 L["S_MAPBAR_OPTIONSMENU_REFRESH"] = "Обновить"
-L["S_MAPBAR_OPTIONSMENU_SHARE"] = "Поделиться аддоном"
 L["S_MAPBAR_OPTIONSMENU_SOUNDENABLED"] = "Включить звуки"
 L["S_MAPBAR_OPTIONSMENU_STATUSBARANCHOR"] = "Привязать к верху"
 L["S_MAPBAR_OPTIONSMENU_TOMTOM_WPPERSISTENT"] = "Постоянная путевая точка"
@@ -90,6 +94,44 @@ L["S_MAPBAR_SUMMARYMENU_NOATTENTION"] = [=[Нет заданий для отсл
 у которых осталось на выполнение менее 2х часов!]=]
 L["S_MAPBAR_SUMMARYMENU_REQUIREATTENTION"] = "Требует внимания"
 L["S_MAPBAR_SUMMARYMENU_TODAYREWARDS"] = "Награды за сегодня"
+--[[Translation missing --]]
+--[[ L["S_MAPFRAME_ALIGN_CENTER"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_MAPFRAME_ALIGN_DESC"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_MAPFRAME_ALIGN_LEFT"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_MAPFRAME_ALIGN"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_MAPFRAME_ERROR_SCALING_DISABLED"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_MAPFRAME_SCALE"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_MAPFRAME_SCALE_ENABLED"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_QUESTBLACKLIST"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_RESET"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_SHOWFACTIONS"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_TIMELEFT_NOPRIORITY"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_TRACKER_RESETPOSITION"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_WORLD_ANCHOR_LEFT"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_WORLD_ANCHOR_RIGHT"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_WORLD_DECREASEICONSPERROW"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_WORLD_INCREASEICONSPERROW"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_WORLD_ORGANIZE_BYMAP"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_WORLD_ORGANIZE_BYTYPE"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_OPTIONS_ZONE_SHOWONLYTRACKED"] = ""--]] 
 L["S_OVERALL"] = "Всего"
 L["S_PARTY"] = "Группа"
 L["S_PARTY_DESC1"] = "Если квест помечен синей звездочкой - он есть у всех игроков в вашей группе."
@@ -118,9 +160,6 @@ L["S_RAREFINDER_TOOLTIP_REMOVE"] = "Убрать"
 L["S_RAREFINDER_TOOLTIP_SEACHREALM"] = "Поиск по другим мирам"
 L["S_RAREFINDER_TOOLTIP_SPOTTEDBY"] = "Обнаружил(а)"
 L["S_RAREFINDER_TOOLTIP_TIMEAGO"] = "минут назад"
-L["S_SHAREPANEL_THANKS"] = [=[Спасибо что поделились World Quest Tracker! 
-Отправьте ссылку на наш аддон своим друзьям в твиттере, фэйсбуке или в Кремле :)]=]
-L["S_SHAREPANEL_TITLE"] = "Тем, кто любит рок!"
 L["S_SUMMARYPANEL_EXPIRED"] = "ИСТЕКШИЕ"
 L["S_SUMMARYPANEL_LAST15DAYS"] = "Последние 15 дней"
 L["S_SUMMARYPANEL_LIFETIMESTATISTICS_ACCOUNT"] = "Статистика аккаунта"
@@ -128,10 +167,28 @@ L["S_SUMMARYPANEL_LIFETIMESTATISTICS_CHARACTER"] = "Статистика пер�
 L["S_SUMMARYPANEL_OTHERCHARACTERS"] = "Другие персонажи"
 L["S_TUTORIAL_AMOUNT"] = "показывает количество или уровень награды"
 L["S_TUTORIAL_CLICKTOTRACK"] = "Кликните, чтобы отслеживать задание"
+--[[Translation missing --]]
+--[[ L["S_TUTORIAL_MAPALIGN"] = ""--]] 
 L["S_TUTORIAL_PARTY"] = "Синей звездочкой помечаются те квесты, которые есть у каждого члена группы!"
 L["S_TUTORIAL_TIMELEFT"] = "показывает оставшееся время (+4 часа, +90 минут, +30 минут, менее 30 минут)"
+--[[Translation missing --]]
+--[[ L["S_TUTORIAL_WORLDBUTTONS"] = ""--]] 
 L["S_TUTORIAL_WORLDMAPBUTTON"] = "Эта кнопка открывает карту Расколотых островов."
 L["S_UNKNOWNQUEST"] = "Неизвестное задание"
+--[[Translation missing --]]
+--[[ L["S_WORLDBUTTONS_SHOW_NONE"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_WORLDBUTTONS_SHOW_TYPE"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_WORLDBUTTONS_SHOW_ZONE"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_WORLDBUTTONS_TOGGLE_QUESTS"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_WORLDMAP_QUESTLOCATIONS"] = ""--]] 
+--[[Translation missing --]]
+--[[ L["S_WORLDMAP_QUESTSUMMARY"] = ""--]] 
 L["S_WORLDMAP_TOOGLEQUESTS"] = "Вкл/Выкл - Задания"
+--[[Translation missing --]]
+--[[ L["S_WORLDMAP_TOOLTIP_TRACKALL"] = ""--]] 
 L["S_WORLDQUESTS"] = "Локальные задания"
 
