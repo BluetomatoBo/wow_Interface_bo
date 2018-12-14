73f9a3fd7da27aa017becf3a77a0e6acd88e6227
bluetomatobo <bo@clargia.org>
2018-12-14 21:29:35 +0800

    GoGoMount 更新到 8.1.3

--- GoGoMount/GoGoMountData.lua
+++ GoGoMount/GoGoMountData.lua
@@ -953,6 +953,9 @@ function GoGo_GetMountDB()
 
 		-- 8.1.0.28616
 		[281554] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Meat Wagon
+
+		-- 8.1.0.28724
+		[290133] = {[9] = true, [38] = true, [300]=true, [301]=true, [330]=true, [400]=true, [402]=true, [403]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10003]=250, [10004]=67},  -- Vulpine Familiar
 		
 --		[123456] = {[38] = true, [330]=true, [400]=true, [402]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10004]=67},  -- Standard Ground Mount template
 --		[123456] = {[9] = true, [38] = true, [300]=true, [301]=true, [330]=true, [400]=true, [402]=true, [403]=true, [405]=true, [701]=true, [10001]=67, [10002]=160, [10003]=250, [10004]=67},  -- Standard Air & Ground Mount template
