045041e98de35dc65451cd5a75ee40379a043547
bluetomatobo <bo@clargia.org>
2019-01-10 20:40:34 +0800

    Pawn 更新到 2.2.33

--- Pawn/PawnUI.lua
+++ Pawn/PawnUI.lua
@@ -1,6 +1,6 @@
 ﻿-- Pawn by Vger-Azjol-Nerub
 -- www.vgermods.com
--- © 2006-2018 Green Eclipse.  This mod is released under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 license.
+-- © 2006-2019 Green Eclipse.  This mod is released under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 license.
 -- See Readme.htm for more information.
 --
 -- User interface code
@@ -61,7 +61,12 @@ local PawnUIFrameNeedsScaleSelector = { true, true, true, true, false, false, fa
 function PawnUI_InventoryPawnButton_Move()
 	if PawnCommon.ButtonPosition == PawnButtonPositionRight then
 		PawnUI_InventoryPawnButton:ClearAllPoints()
-		PawnUI_InventoryPawnButton:SetPoint("TOPRIGHT", "CharacterTrinket1Slot", "BOTTOMRIGHT", -1, -8)
+		if PaperDollFrame.ExpandButton then
+			-- DejaCharacterStats compatibility
+			PawnUI_InventoryPawnButton:SetPoint("TOPRIGHT", "CharacterTrinket1Slot", "BOTTOMRIGHT", -31, -8)
+		else
+			PawnUI_InventoryPawnButton:SetPoint("TOPRIGHT", "CharacterTrinket1Slot", "BOTTOMRIGHT", -1, -8)
+		end
 		PawnUI_InventoryPawnButton:Show()
 		if PawnUI_InspectPawnButton then
 			PawnUI_InspectPawnButton:ClearAllPoints()
@@ -85,7 +90,6 @@ function PawnUI_InventoryPawnButton_Move()
 		if PawnUI_SocketingPawnButton then
 			PawnUI_SocketingPawnButton:ClearAllPoints()
 			PawnUI_SocketingPawnButton:SetPoint("TOPRIGHT", "ItemSocketingFrame", "TOPRIGHT", -14, -32)
-			--PawnUI_SocketingPawnButton:SetPoint("TOPLEFT", "ItemSocketingFrame", "TOPLEFT", 64, -32)
 			PawnUI_SocketingPawnButton:Show()
 		end
 	else
